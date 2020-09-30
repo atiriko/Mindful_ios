b09d16f8c880e9b5f0c8794363f1c19db1f4b5b4
Atahan Sahlan <atirikosahlan@gmail.com>
2020-10-01 00:09:34 +0100

    Initial Commit

---
+++ Motivation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Motivation
+//
+//  Created by Atahan Sahlan on 01/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
